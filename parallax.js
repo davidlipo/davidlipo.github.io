62ebe8de54749f1924e5e1510758b43568d265ec
David Lipowicz <dlipowicz@hotmail.com>
2017-10-02 17:12:34 +0100

    Fade when close

--- parallax.js
+++ parallax.js
@@ -5,7 +5,8 @@ window.onload = function(){
 function onScroll(event) {
     var screenHeight = window.innerHeight;
     var offset = window.scrollY;
-    var scrollableWindowHeight = document.getElementById("content").scrollHeight - screenHeight;
+    var contentHeight = document.getElementById("content").scrollHeight;
+    var scrollableWindowHeight = contentHeight - screenHeight;
     var percentageScrolled = offset / scrollableWindowHeight;
     var parallaxImage = document.getElementById("parallax-image");
     var imageScrollableHeight = parallaxImage.height - screenHeight;
@@ -20,4 +21,19 @@ function onScroll(event) {
     debug += "imageScrollableHeight " + imageScrollableHeight + "<br/>";
     debug += "amountToScroll " + amountToScroll + "<br/>";
     document.getElementById("debug").innerHTML = debug;*/
+
+    var distanceToTop = document.getElementById("fadeIn").getBoundingClientRect().top;
+    if (distanceToTop < screenHeight) {
+        changeColorOnScreen();
+        var distanceToEnd = 200;
+        var white = parseInt("FFFFFF", 16);
+        var yellow = parseInt("FFFF00", 16);
+        var percentageToGoal = Math.min(1, (screenHeight - distanceToTop) / (screenHeight - distanceToEnd));
+        var color = Math.ceil(percentageToGoal*(white - yellow) + yellow);
+        document.getElementById("fadeIn").style.backgroundColor = "#" + color.toString(16);
+    }
+}
+
+function changeColorOnScreen(element, distanceToEnd) {
+
 }
